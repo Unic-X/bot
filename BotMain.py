3e1533a51825bcc26b82184f9cf01fe9bd63038b
Terroriser747 <70016341+Terroriser747@users.noreply.github.com>
2020-11-14 18:25:50 +0530

    Update BotMain.py

--- BotMain.py
+++ BotMain.py
@@ -12,6 +12,14 @@ async def on_message(message):
         await message.channel.send("hi")
     elif message.content.find("!mc") !=-1:
         await message.channel.send("tera baap mc ")
-    
+        
+@client.event
+async def on_message_delete(message):                       #for logging deleted messages
+    if message.guild.id == 485358227249168396:
+         ma=message.author
+         mc=message.content
+         id=777150786974515200
+         channel=client.get_channel(id)
+         await channel.send(f'{mc} | Author:{ma}')
 
 client.run(token)
