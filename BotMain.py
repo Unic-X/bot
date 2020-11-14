8d079eaac0a074e0dd756917e1349e9b6b7934e3
Unic X <73340774+Unic-X@users.noreply.github.com>
2020-11-14 11:14:27 +0530

    Update BotMain.py

--- BotMain.py
+++ BotMain.py
@@ -1,1 +1,17 @@
-print('Your mom gay')
+import discord as dc
+import asyncio as asy
+
+token = "NzY4ODI1NjM1NjgyMjU0ODY5.X5GGqw.pO0vRZ_P4vBhzoj0qlq5fdFNsu4"
+dc.Emoji
+client = dc.Client()
+id=client.get_guild(768825635682254869)
+
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") !=-1:
+        await message.channel.send("hi")
+    elif message.content.find("!mc") !=-1:
+        await message.channel.send("tera baap mc ")
+    
+
+client.run(token)
